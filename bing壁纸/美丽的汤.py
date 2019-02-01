cca017e5b1d0c3d39fa9b3f9373a83d325f723de
Your Name <you@example.com>
2019-02-01 13:13:29 +0800

    1

--- bing壁纸/美丽的汤.py
+++ bing壁纸/美丽的汤.py
@@ -1,22 +1,41 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import sys
+import os
+# log日志记录
+def log(flag, Image_path, Image_Url):
+    print ("文件网址 ：", Image_Url)
+    print ("文件路径 ：", Image_path)
+    if flag == 2:
+        print ("Successful")
+    else:
+        print ("Unsuccessful")
+        
+flag = int(0)
+# 文件所在目录
+path = os.path.split( os.path.realpath( sys.argv[0] ) )[0]
 
 # 获取图片url
 try:
     Page = urllib.request.urlopen('https://www.bing.com/?mkt=zh-CN&mkt=zh-CN').read()
     Soup = BeautifulSoup(Page,"lxml")
+    flag = 1
 except:
-    print ("Error")
-    sys.exit()
+    print ("Url Error")
 Temp = Soup.link.attrs['href']
 Image_Url = "https://cn.bing.com" + Temp
-print (Image_Url)
-
 # 保存图片
-## 名字就用 /az/hprichbg/rb/###.jpg 中的 ###.jpg 来命名吧
-name = Temp.split('/')[4]
-print (name)
-urllib.request.urlretrieve(Image_Url, name)
+name = Temp.split('/')[4]   # 名字就用 /az/hprichbg/rb/###.jpg 中的 ###.jpg 来命名吧
+# urllib.request.urlretrieve(Image_Url, name)
+response = urllib.request.urlopen(Image_Url)
+htmldata = response.read()
+Image_path = path + '\\' + name
+try:
+    fileOb = open(Image_path, 'wb')     #打开一个文件，没有就新建一个
+    fileOb.write(htmldata)
+    fileOb.close()
+    flag = 2;
+except:
+    print ("Save Error")
 
-# log日志记录
+log(flag, Image_path, Image_Url)
