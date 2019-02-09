b9b873d0966ca7707393969bf60e3edd42e91f79
Your Name <you@example.com>
2019-02-09 20:10:09 +0800

    1

--- bing壁纸/爬取网页.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-page = urllib.request.urlopen('https://www.bing.com/?mkt=zh-CN&mkt=zh-CN').read()
-print (page)
-fhandle = open("./1.txt","wb")    #将爬取的网页保存在本地
-fhandle.write(page)
-fhandle.close()
