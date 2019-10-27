5eec7d4b5791e06862c406cc0a2c167f1abf4fbc
Your Name <gyh108wy@163.com>
2019-10-27 17:17:24 +0800

    1

--- go/calc/calc.go
+++ go/calc/calc.go
@@ -3,17 +3,17 @@ package main
 import (
 	"os"
 	"fmt"
-	"simplemath"
+	"../simplemath"
 	"strconv"
 )
 
-func Usage = func() {
+var Usage = func() {
 	fmt.Println("Usage : calc command [arguments] ...")
 	fmt.Println("\nThe commands are:\n\tadd\tAddition of two values.\n\tsqrt\tSquare root of a non-negative value.")
 }
 
 func main() {
-	args := os.args[1:]
+	args := os.Args[1:]
 	if args == nil || len(args) < 2{
 		Usage()
 		return
@@ -25,6 +25,27 @@ func main() {
 				fmt.Println("Usage: calc add <interger1><interger2>")
 				return
 			}
-			v1, err1 := strconv.Atoi(args)
+			v1, err1 := strconv.Atoi(args[1])
+			v2, err2 := strconv.Atoi(args[2])
+			if err1 != nil || err2 != nil {
+				fmt.Println("Usage: calc add <int1><int2>")
+				return
+			}
+			ret := simplemath.Add(v1, v2)
+			fmt.Println("Usage: ", ret)
+		case "sqrt":
+			if len(args) != 2 {
+				fmt.Println("Usage: calc add <interger1><interger2>")
+				return
+			}
+			v, err := strconv.Atoi(args[1])
+			if err != nil {
+				fmt.Println("Usage: calc sqrt <int>")
+				return
+			}
+			ret := simplemath.Sqrt(v)
+			fmt.Println("Result: ", ret)
+		default:
+			Usage()
 	}
 } 
