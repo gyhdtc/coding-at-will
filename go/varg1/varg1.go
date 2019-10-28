47a84f550b982aa8b248cf7fab2c437a3e0bf53a
Your Name <gyh108wy@163.com>
2019-10-28 16:45:40 +0800

    1

--- go/varg1/varg1.go
+++ go/varg1/varg1.go
@@ -1,1 +1,26 @@
-package varg1
+package main
+
+import "fmt"
+
+func MyPrint(args ...interface{}) {
+	for _, arg := range args {
+		switch arg.(type) {
+		case int :
+			fmt.Println("int.")
+		case string :
+			fmt.Println("string.")
+		case int64 :
+			fmt.Println("int64.")
+		default:
+			fmt.Println("unknown.")
+		}
+	}
+}
+
+func main() {
+	var v1 int = 1
+	var v2 string = "2"
+	var v3 int64 = 234
+	var v4 float64 = 3.14
+	MyPrint(v1, v2, v3, v4)
+}
