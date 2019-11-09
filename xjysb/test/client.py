c03fe69ab05b6d78e9c2f89ff690f4aef508e113
Your Name <gyh108wy@163.com>
2019-11-10 01:05:56 +0800

    1

--- xjysb/test/client.py
+++ xjysb/test/client.py
@@ -2,10 +2,12 @@ import socket
 import string
 import threading
 import time
-
+flag = 0
+BUFSIZE = 1024
 def send(server):
-    while True:
-        data = input('>')
+    global flag
+    while flag == 0:
+        data = input('>>> ')
         if data != "exit" and data != "":
             server.send(bytes(data, encoding="utf-8"))
             print ("[send] ", data)
@@ -13,21 +15,24 @@ def send(server):
             server.send(bytes(data, encoding="utf-8"))
             print ("[send] ", data)
             print ("[END]")
+            flag = 1
             break
 
 def rec(client):
-    while True:
+    global flag
+    while flag == 0:
         try:
             RecData = str(client.recv(BUFSIZE), "utf-8")
             print("[rec] ", RecData)
             if RecData == "exit":
                 print("[END]")
+                flag = 1
                 break
         except:
+            flag = 1
             break
         
 # 用 client 连接
-BUFSIZE = 1024
 MyClient = socket.socket()
 FileServerHost = "127.0.0.1"    # 获取 文件 服务器主机名
 FileServerPort = 12345              # 设置 文件 服务器端口号
@@ -39,5 +44,8 @@ t1.start()
 t2 = threading.Thread(target=rec, args=(MyClient,))
 t2.start()
 
-time.sleep(1000)
+t1.join()
+t2.join()
+
 MyClient.close()
+print("h")
