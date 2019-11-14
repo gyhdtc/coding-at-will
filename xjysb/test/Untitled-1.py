9f9852379b4b02b9ae2b22f1bcffe757c577233f
Your Name <gyh108wy@163.com>
2019-11-15 01:35:02 +0800

    1

---
+++ xjysb/test/Untitled-1.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+a = deque()
+
+a.append("gyh")
+a.append("ggg")
+print(a)
+a.remove("ggg")
+print(a)
--- xjysb/test/condition.py
+++ xjysb/test/condition.py
@@ -3,19 +3,20 @@ import time
 
 condition = threading.Condition()    # 创建condition对象
 
-def func():
+def func(i):
+    print("in func..", i)
     condition.acquire()    # 如果没有with语句，必写这句，否者报错
     condition.wait()       # 阻塞，等待其他线程调用notify()
-    print("in func..")
+    print("in func..", i)
     condition.release()    # 与acquire()成对出现
 
 # 启10个线程
 for i in range(10):
-    t = threading.Thread(target=func, args=())
+    t = threading.Thread(target=func, args=(i,))
     t.start()
 
 time.sleep(5)
 
 condition.acquire()
-condition.notify(2)        # 通知两个线程执行
+condition.notify(10)        # 通知两个线程执行
 condition.release()
